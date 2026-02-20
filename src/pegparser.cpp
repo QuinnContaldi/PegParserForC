71662a42c8df75485850d96b0d5ed3d3f7f7e7c7
QuinnContaldi <qcontaldi@unr.edu>
2026-02-19 23:01:07 -0800

    We got things building and catgirl is being recognized

--- src/pegparser.cpp
+++ src/pegparser.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
+#include <cassert>
 #include <peglib.h>
 
-int main()
-{
-    std::cout << "Hello Catgirl \n";
+int main() {
+  peg::parser p(R"(
+    Start <- 'Catgirl' End
+    End   <- !.
+    %whitespace <- [ \t\r\n]*
+  )");
+
+  assert(p);
+
+  p.enable_packrat_parsing();
+
+  assert(p.parse("Catgirl"));
+  assert(p.parse("   Catgirl"));
+  assert(p.parse("Catgirl   "));
+  assert(p.parse("\n\t Catgirl \r\n"));
+
+  // Still rejects extra tokens:
+  assert(!p.parse("Catgirl meow"));
+  assert(!p.parse("SuperCatgirl"));
+  std::cout << "OK\n";
 }
-
-/**
- * @brief 
- * 
- My current goals
-Parse: int main() {}
-Parse: int main(){ return 0; }
-Parse: int main(){ ; ; return 0; } (empty statements + return)
-Token rules (identifiers, keywords, literals, operators, comments, whitespace)
-
-CompoundStatement, If/While/For, Return, ;, expression statements
-
-Expression precedence ladder (this part is easy for you)
-
-THEN add declarations with DeclStart guard
-
-THEN typedef tracking
-
-PEG parser for C bootstrap plan.
- */
